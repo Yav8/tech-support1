2ad93a6766359760df4e0110f32fe4eaebf0319b
Yav <yavieron@hotmail.es>
2018-02-15 23:49:22 +0100

    Modificada clase Responder para que proporcione diferentes respuestas basadas también en conjuntos de palabras clave

--- Responder.java
+++ Responder.java
@@ -15,7 +15,13 @@ public class Responder
 {
     private Random aleatorio;
     private ArrayList<String> listaRespuestas;
-    private HashMap<String, String> mapaDeRespuestas;
+    private HashMap<HashSet<String>, String> mapaDeRespuestas;
+    private HashSet<String> conjuntoDePalabras1;
+    private HashSet<String> conjuntoDePalabras2;
+    private HashSet<String> conjuntoDePalabras3;
+    private HashSet<String> conjuntoDePalabras4;
+    private HashSet<String> conjuntoDePalabras5;
+    private HashSet<String> conjuntoDePalabras6;
 
     /**
      * Construct a Responder
@@ -23,20 +29,42 @@ public class Responder
     public Responder()
     {
         aleatorio = new Random();
-        listaRespuestas = new ArrayList<String>();
         
+        listaRespuestas = new ArrayList<String>();        
         listaRespuestas.add("What's your problem?");
         listaRespuestas.add("What? I can't believe it.");
         listaRespuestas.add("You have serious problems with this...");
         listaRespuestas.add("Let's do this.");
         listaRespuestas.add("Yeah, we did it!");
         
-        mapaDeRespuestas = new HashMap<String, String>();
+        conjuntoDePalabras1 = new HashSet<String>();
+        conjuntoDePalabras1.add("slow");
         
-        mapaDeRespuestas.put("slow", "I guess, the performance of your computer is not working as expected.");
-        mapaDeRespuestas.put("crash", "Oh my god! What have you done?!");
-        mapaDeRespuestas.put("idea", "Then tell me, I am curious.");
-        mapaDeRespuestas.put("worst", "There are worse computers for sure...");
+        conjuntoDePalabras2 = new HashSet<String>();
+        conjuntoDePalabras2.add("crash");
+        
+        conjuntoDePalabras3 = new HashSet<String>();
+        conjuntoDePalabras3.add("idea");
+        
+        conjuntoDePalabras4 = new HashSet<String>();
+        conjuntoDePalabras4.add("worst");
+        
+        conjuntoDePalabras5 = new HashSet<String>();
+        conjuntoDePalabras5.add("slow");
+        conjuntoDePalabras5.add("crash");
+        
+        conjuntoDePalabras6 = new HashSet<String>();
+        conjuntoDePalabras6.add("slow");
+        conjuntoDePalabras6.add("crash");
+        conjuntoDePalabras6.add("worst");
+        
+        mapaDeRespuestas = new HashMap<HashSet<String>, String>();        
+        mapaDeRespuestas.put(conjuntoDePalabras1, "I guess, the performance of your computer is not working as expected.");
+        mapaDeRespuestas.put(conjuntoDePalabras2, "Oh my god! What have you done?!");
+        mapaDeRespuestas.put(conjuntoDePalabras3, "Then tell me, I am curious.");
+        mapaDeRespuestas.put(conjuntoDePalabras4, "There are worse computers for sure...");
+        mapaDeRespuestas.put(conjuntoDePalabras5, "Wow, it looks like your computer is untouchable...");
+        mapaDeRespuestas.put(conjuntoDePalabras6, "I'm sorry, but I think you should buy a new computer...");
     }
 
     /**
@@ -45,18 +73,9 @@ public class Responder
      */
     public String generateResponse(HashSet<String> texto)
     {
-        String respuesta = "";
-        Iterator<String> it = texto.iterator();
-        boolean hayCoincidencia = false;
+        String respuesta = mapaDeRespuestas.get(texto);
         
-        while(it.hasNext() && hayCoincidencia == false) {
-            respuesta = mapaDeRespuestas.get(it.next());
-            if(respuesta != null) {
-                hayCoincidencia = true;
-            }
-        }
-        
-        if(hayCoincidencia == false) {
+        if(respuesta == null) {
             respuesta = listaRespuestas.get(aleatorio.nextInt(listaRespuestas.size()));
         }
         
